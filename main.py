0fa2ef9a7a049838455f8d7c34c3a35020b40d74
ajaykm000563 <ajaykm000563@gmail.com>
2026-02-16 21:41:17 +0530

    Data Validation Completed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.entity.config_entity import DataValidationConfig
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.entity.artifact_entity import DataValidationArtifact
 from networksecurity.exception.exception import CustomException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig
@@ -14,7 +17,13 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Exporting collection as DataFrame")
         dataingetionartifact = data_ingestion.initiate_data_ingestion()
-        print(dataingetionartifact)
+        
+        logging.info("Starting data validation")
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        datavalidation = DataValidation(datavalidationconfig, dataingetionartifact)
+        datavalidationartifact = datavalidation.initiate_data_validation()
+        logging.info("Data validation completed successfully")
+        print(datavalidationartifact)
     except Exception as e:
         logging.error(f"Error in main execution: {e}")
         raise CustomException(e, sys)
--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,11 +1,18 @@
 import sys
 from networksecurity.logging.logger import logging
+
+
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
+
+    # If exception is manually raised (no traceback exists)
+    if exc_tb is None:
+        return f"Error message: {str(error)}"
+
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-    error_message = f"Error occurred in file: {file_name} at line number: {line_number} with error message: {str(error)}"
-    return error_message
+
+    return f"Error occurred in file: {file_name} at line number: {line_number} with error message: {str(error)}"
 
 
 class CustomException(Exception):
@@ -14,7 +21,8 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message    
+        return self.error_message
+
 
 if __name__ == "__main__":
     try:
